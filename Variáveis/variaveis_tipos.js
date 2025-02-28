9b91afd3809e9d6e0d2a6202732dce74467a18e5
CaduTartuce <caduoliveira.tartuce@gmail.com>
2025-02-28 10:59:54 -0300

    add atividade1

---
+++ Variáveis/variaveis_tipos.js
@@ -0,0 +1,22 @@
+const  n = 1;
+
+
+
+var j =3;
+j = 4;
+
+let nome = "Cadu ";
+let telefone = "40028922";
+
+let x = 3.14;
+x = 0.5;
+x = 1;
+
+let b = true;
+b = flase;
+
+let c = -1;
+
+let conexao = conexao();
+
+console.log(`b=${b}`);
---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+let xSoma = 1;
+let ySoma = 2;
+let zSoma = xSoma + ySoma;
+
+let xSubtracao = 12;
+let ySubtracao = 8;
+let zSubtracao = xSubtracao - ySubtracao;
+
+let xMultiplicacao = 10;
+let yMultiplicacao = 20;
+let zMultiplicacao = xMultiplicacao * yMultiplicacao;
+
+let xDivisao = 10;
+let yDivisao = 2;
+let zDivisao = xDivisao / yDivisao;
+
+console.log(`Reultado Soma entra ${xSoma} e ${ySoma} e igual a:  ${zSoma}`);
+console.log(`Reultado Subtracao entre ${xSubtracao} e ${ySubtracao} e igual a:  ${zSubtracao}`);
+console.log(`Reultado Multiplicacao entre ${xMultiplicacao} e ${yMultiplicacao} e igual a:  ${zMultiplicacao}`);
+console.log(`Reultado Divisao entra ${xDivisao} e ${yDivisao} e igual a:  ${zDivisao}`);
